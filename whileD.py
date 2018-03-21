54caaf98f94b6dda3100ba43fec1bc2a9739b9ab
cerenurken <36957603+cerenurken@users.noreply.github.com>
2018-03-21 21:15:59 +0300

    Add files via upload

---
+++ whileD.py
@@ -0,0 +1,68 @@
+#1.SORU
+satisM=500
+birimM=20
+ciro=5000
+i=1
+while True:
+    satisM=(satisM+200)*12
+    birimM=(birimM+10)*12
+    ciro=satisM+birimM
+    i=i+1
+    if(ciro>500000):
+        print(i,"yıl sonra cironuz 500000 tl yi geçer.")
+        break
+    
+
+#2.SORU
+sm=10000
+satılanurun=500
+alınanurun=100
+i=1
+while True:
+    sm=sm-satılanurun+alınanurun
+    i=i+1
+    if(sm==0):
+        print(i,"ay sonra stok miktarı sıfırdır.")
+        break
+
+
+#3.SORU
+print("3 sayısının bölümünden kalan sayıların toplamı")
+print("Çıkış yapmak istiyorsanız 0 sayısını giriniz")
+kalanlar = 0
+while True:
+    sayi = int(input("Sayı Giriniz : "))
+    if(sayi > 0 ):
+        kalanlar = kalanlar+(sayi%3)
+        print("Girilen Sayıların 3 ile bölümünden kalanların toplamı =",kalanlar)
+    else:
+        break     
+        
+
+#4.SORU
+gunluky=90
+mesaisaati=""
+calismasaati=40
+while True:
+    mesaisaati=int(input("mesai saatinizi giriniz:"))
+    if (mesaisaati<22):
+        print("aylık mesai ile birlikte ödenecek miktar:",(40*4*90)+(mesaisaati*gunluky*0.10))
+        break
+    else:
+        print("22 saatten fazla mesai yapılmaz.")
+        break
+
+
+#5.SORU
+gunlukurun=200
+defolumal=""
+toplamdefo=0
+i=0
+while True:
+    defolumal=int(input("Günlük defolu ürün sayısınız giriniz:"))
+    toplamdefo=toplamdefo+defolumal
+    i=i+1
+    print(i,"günlük toplam defolu ürün sayınız:",toplamdefo)
+    if(defolumal>(gunlukurun*0.20)):
+        print("Defolu ürün sayınız günlük mal üretiminizin %20'nin üzerindedir")
+        
